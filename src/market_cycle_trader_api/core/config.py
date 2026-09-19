0fbf7f26a83c268482782122f9e066eed40857d7
betovlima <betovlima@gmail.com>
2026-09-19 00:43:48 -0300

    chore: bump API version to 10.8.61

--- src/market_cycle_trader_api/core/config.py
+++ src/market_cycle_trader_api/core/config.py
@@ -4,7 +4,7 @@ import os
 from pathlib import Path
 from typing import Any
 
-API_VERSION = "10.8.60"
+API_VERSION = "10.8.61"
 ENGINE_MODULE = "market_cycle_trader_api.engine.compound_rotation_backtest"
 PACKAGE_DIR = Path(__file__).resolve().parents[1]
 SOURCE_ROOT = PACKAGE_DIR.parent
